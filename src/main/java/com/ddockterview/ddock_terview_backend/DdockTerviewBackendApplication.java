12c1b38a1f2789ece9b91ba728e960f5a26c2990
Quintillion0113 <awcrgym@gmail.com>
2025-10-04 23:28:16 +0900

    feat: Initial commit of Spring Boot project structure

---
+++ src/main/java/com/ddockterview/ddock_terview_backend/DdockTerviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ddockterview.ddock_terview_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdockTerviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdockTerviewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddockterview/ddock_terview_backend/DdockTerviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddockterview.ddock_terview_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdockTerviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
