5f38f55fae52a2baa7ea7adff129cfc20cb94423
Quintillion0113 <awcrgym@gmail.com>
2025-10-11 18:53:49 +0900

    WebConfig 생성 (vercel 연동)

---
+++ src/main/java/com/ddockterview/ddock_terview_backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ddockterview.ddock_terview_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // "/**"는 모든 경로에 대한 API 요청을 의미합니다.
+                .allowedOrigins("https://ddock-terview-frontend.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드 종류
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 자격 증명(쿠키 등) 허용
+    }
+}
