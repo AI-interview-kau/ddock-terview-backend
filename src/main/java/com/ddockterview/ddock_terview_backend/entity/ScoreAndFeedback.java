8cb703121ca6439e6e445bdaeda31deae6d7fd2e
chadoli27 <jasminekim1009@gmail.com>
2025-10-30 22:20:07 +0900

    ScoreAndFeedback 엔티티 생성

---
+++ src/main/java/com/ddockterview/ddock_terview_backend/entity/ScoreAndFeedback.java
@@ -0,0 +1,54 @@
+package com.ddockterview.ddock_terview_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ScoreAndFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scoreId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Session session;
+
+    private int totalScore;
+    private int suitability;
+    private int intendUnderstanding;
+    private int problemSolving;
+    private int accuracy;
+    private int experience;
+    private int logicality;
+    private int confidence;
+    private int speed;
+    private int voice;
+    private int gesture;
+    private int attitude;
+    private int gazing;
+
+    @Column(columnDefinition = "TEXT")
+    private String generalFeedback;
+
+    @Column(columnDefinition = "TEXT")
+    private String pro;
+
+    @Column(columnDefinition = "TEXT")
+    private String con;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+
+
+}
