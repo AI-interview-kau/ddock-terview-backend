114ff2aabe291bf3809faa976ac53b22faf716fb
chadoli27 <jasminekim1009@gmail.com>
2025-10-30 19:25:00 +0900

    질문 선택

---
+++ src/main/java/com/ddockterview/ddock_terview_backend/repository/QuestionAfterRepository.java
@@ -0,0 +1,7 @@
+package com.ddockterview.ddock_terview_backend.repository;
+
+import com.ddockterview.ddock_terview_backend.entity.QuestionAfter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionAfterRepository extends JpaRepository<QuestionAfter, Long> {
+}
