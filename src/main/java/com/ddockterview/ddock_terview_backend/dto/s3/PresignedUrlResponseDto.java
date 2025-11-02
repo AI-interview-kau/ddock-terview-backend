3d72717515e919edea352b8f00fcb6a06506496d
Quintillion0113 <awcrgym@gmail.com>
2025-11-02 19:19:23 +0900

    IDE 업데이트 전 중간 저장(오류 있음)

---
+++ src/main/java/com/ddockterview/ddock_terview_backend/dto/s3/PresignedUrlResponseDto.java
@@ -0,0 +1,11 @@
+package com.ddockterview.ddock_terview_backend.dto.s3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PresignedUrlResponseDto {
+    private String s3Key;
+    private String uploadUrl;
+}
