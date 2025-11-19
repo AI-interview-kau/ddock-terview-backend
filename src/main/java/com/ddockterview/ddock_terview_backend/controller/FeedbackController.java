0bc65dc6a5ce32c92d41a61e01d32a776c2db5b0
chadoli27 <jasminekim1009@gmail.com>
2025-11-19 14:46:12 +0900

    다시되돌리기

--- src/main/java/com/ddockterview/ddock_terview_backend/controller/FeedbackController.java
+++ src/main/java/com/ddockterview/ddock_terview_backend/controller/FeedbackController.java
@@ -1,12 +1,14 @@
 package com.ddockterview.ddock_terview_backend.controller;
 
 import com.ddockterview.ddock_terview_backend.dto.GlobalResponseDto;
+import com.ddockterview.ddock_terview_backend.dto.feedback.FeedbackRequestDto;
 import com.ddockterview.ddock_terview_backend.service.FeedbackService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
@@ -17,11 +19,11 @@ public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
-    @PostMapping(value = "/feedback", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<GlobalResponseDto> saveFeedback(@RequestParam("file") MultipartFile file) throws IOException {
-        System.out.println(">>> /api/feedback called with a file");
+    @PostMapping("/feedback")
+    public ResponseEntity<GlobalResponseDto> saveFeedback(@RequestBody FeedbackRequestDto requestDto) throws IOException {
+        System.out.println(">>> /api/feedback called with JSON body");
 
-        feedbackService.saveAllFeedback(file);
+        feedbackService.saveAllFeedback(requestDto);
         GlobalResponseDto response = new GlobalResponseDto(200, "모든 피드백을 성공적으로 저장했습니다.");
         return ResponseEntity.ok(response);
     }
